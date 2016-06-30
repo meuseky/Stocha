a769de76e596c002aeb6c6de265ad4497d3638d0
meuseky <kemeuse@gmail.com>
2016-07-01 00:05:54 +0100

    Initial Commit

---
+++ html/src/com/stocha/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.stocha.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.stocha.game.StochaGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new StochaGame();
+        }
+}
